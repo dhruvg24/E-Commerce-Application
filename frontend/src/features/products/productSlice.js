cb2735d24f54690b7dd878dfbb7a592f45ec71f7
dhruvg24 <dhruvgarg24112002@gmail.com>
2025-07-20 16:58:30 +0530

    redux setup and products display on home page

---
+++ frontend/src/features/products/productSlice.js
@@ -0,0 +1,64 @@
+import {createSlice,createAsyncThunk} from '@reduxjs/toolkit'
+import axios from 'axios'
+
+
+
+export const getProduct = createAsyncThunk('product/getProduct', async(_,{rejectWithValue})=>{
+    // to handle errors
+    try{
+        // Access API link[backend]
+        const link = '/api/products';
+        // get request
+        const {data} = await axios.get(link);
+        console.log('Response:',data);
+
+        return data;
+
+
+    }catch(err){
+        return rejectWithValue(err.response?.data || 'Some error occurred');
+        // from backend error
+    }
+})
+const productSlice = createSlice({
+    name:'product',
+    initialState: {
+        products: [],
+        productCount: 0,
+        loading:false,
+        error:null
+    },
+    reducers:{
+        removeErrors:(state)=>{
+            state.error = null;
+        },
+        // while working with APIs the errors if any should be shown to user and to be removed after some time.
+    },
+    // lifecycle actions
+    extraReducers:(builder)=>{
+        builder.addCase(getProduct.pending, (state)=>{
+            state.loading=true
+            state.error = null
+            // load spinners in UI
+        })
+        .addCase(getProduct.fulfilled,(state,action)=>{
+            console.log('Fulfilled action payload', action.payload)
+            state.loading = false;
+            state.error =null;
+            state.products = action.payload.products; //from backend
+            state.productCount = action.payload.productCount;
+        })
+        .addCase(getProduct.rejected, (state,action)=>{
+            state.loading=false;
+            state.error = action.payload || 'Something went wrong';
+        })
+    }
+
+})
+
+export const {removeErrors} = productSlice.actions;
+// removeErros -> action creator
+export default productSlice.reducer;
+
+// reducers defind how the state changes when an action is dispatched...
+
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,102 +1,115 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Footer from '../components/Footer'
 import '../pageStyles/Home.css'
 import Navbar from '../components/Navbar'
 import ImageSlider from '../components/ImageSlider'
 import Product from '../components/Product'
+import PageTitle from '../components/PageTitle'
+import { useDispatch, useSelector } from 'react-redux'
+import { getProduct } from '../features/products/productSlice.js'
 
-const products= [
-        {
-            "_id": "68779004414a288bc475f2dd",
-            "name": "Product 1",
-            "description": "Product Description 1",
-            "price": 300,
-            "ratings": 4,
-            "image": [
-                {
-                    "public_id": "test_id",
-                    "url": "test_url",
-                    "_id": "68779004414a288bc475f2de"
-                }
-            ],
-            "category": "test category 1",
-            "stock": -2,
-            "numOfReviews": 1,
-            "reviews": [
-                {
-                    "user": "6878fe27aead1c993eaeab7b",
-                    "name": "Dhruv Garg",
-                    "rating": 4,
-                    "comment": "awesome",
-                    "_id": "687a17e8c5459c6e5bcfc65a"
-                }
-            ],
-            "createdAt": "2025-07-16T11:41:56.244Z",
-            "__v": 1
-        },
-        {
-            "_id": "6878f106e8011276e841cd57",
-            "name": "Product 2",
-            "description": "Product Description 2",
-            "price": 400,
-            "ratings": 0,
-            "image": [
-                {
-                    "public_id": "test_id_2",
-                    "url": "test_url_2",
-                    "_id": "6878f106e8011276e841cd58"
-                }
-            ],
-            "category": "trouser",
-            "stock": 1,
-            "numOfReviews": 0,
-            "reviews": [
-                {
-                    "name": "test user 2",
-                    "rating": 3,
-                    "comment": "test comment xyz",
-                    "_id": "6878f106e8011276e841cd59"
-                }
-            ],
-            "user": "6878bb86e88908875662cb32",
-            "createdAt": "2025-07-17T12:48:06.714Z",
-            "__v": 0
-        },
-        {
-            "_id": "6879186ce3de0fe746c79a75",
-            "name": "Product 3",
-            "description": "Product Description 3",
-            "price": 600,
-            "ratings": 0,
-            "image": [
-                {
-                    "public_id": "test_id_3",
-                    "url": "test_url_3",
-                    "_id": "6879186ce3de0fe746c79a76"
-                }
-            ],
-            "category": "shirts",
-            "stock": 1,
-            "numOfReviews": 0,
-            "reviews": [
-                {
-                    "name": "test user 1",
-                    "rating": 4,
-                    "comment": "test comment",
-                    "_id": "6879186ce3de0fe746c79a77"
-                }
-            ],
-            "user": "6878fe27aead1c993eaeab7b",
-            "createdAt": "2025-07-17T15:36:12.070Z",
-            "__v": 0
-        },
+// const products= [
+//         {
+//             "_id": "68779004414a288bc475f2dd",
+//             "name": "Product 1",
+//             "description": "Product Description 1",
+//             "price": 300,
+//             "ratings": 4,
+//             "image": [
+//                 {
+//                     "public_id": "test_id",
+//                     "url": "test_url",
+//                     "_id": "68779004414a288bc475f2de"
+//                 }
+//             ],
+//             "category": "test category 1",
+//             "stock": -2,
+//             "numOfReviews": 1,
+//             "reviews": [
+//                 {
+//                     "user": "6878fe27aead1c993eaeab7b",
+//                     "name": "Dhruv Garg",
+//                     "rating": 4,
+//                     "comment": "awesome",
+//                     "_id": "687a17e8c5459c6e5bcfc65a"
+//                 }
+//             ],
+//             "createdAt": "2025-07-16T11:41:56.244Z",
+//             "__v": 1
+//         },
+//         {
+//             "_id": "6878f106e8011276e841cd57",
+//             "name": "Product 2",
+//             "description": "Product Description 2",
+//             "price": 400,
+//             "ratings": 0,
+//             "image": [
+//                 {
+//                     "public_id": "test_id_2",
+//                     "url": "test_url_2",
+//                     "_id": "6878f106e8011276e841cd58"
+//                 }
+//             ],
+//             "category": "trouser",
+//             "stock": 1,
+//             "numOfReviews": 0,
+//             "reviews": [
+//                 {
+//                     "name": "test user 2",
+//                     "rating": 3,
+//                     "comment": "test comment xyz",
+//                     "_id": "6878f106e8011276e841cd59"
+//                 }
+//             ],
+//             "user": "6878bb86e88908875662cb32",
+//             "createdAt": "2025-07-17T12:48:06.714Z",
+//             "__v": 0
+//         },
+//         {
+//             "_id": "6879186ce3de0fe746c79a75",
+//             "name": "Product 3",
+//             "description": "Product Description 3",
+//             "price": 600,
+//             "ratings": 0,
+//             "image": [
+//                 {
+//                     "public_id": "test_id_3",
+//                     "url": "test_url_3",
+//                     "_id": "6879186ce3de0fe746c79a76"
+//                 }
+//             ],
+//             "category": "shirts",
+//             "stock": 1,
+//             "numOfReviews": 0,
+//             "reviews": [
+//                 {
+//                     "name": "test user 1",
+//                     "rating": 4,
+//                     "comment": "test comment",
+//                     "_id": "6879186ce3de0fe746c79a77"
+//                 }
+//             ],
+//             "user": "6878fe27aead1c993eaeab7b",
+//             "createdAt": "2025-07-17T15:36:12.070Z",
+//             "__v": 0
+//         },
         
-    ]
+//     ]
 
 const Home = () => {
+    // useSelector hook
+    const {loading,error,products,productCount} = useSelector((state)=>
+        state.product);
+
+    // dispatch
+    const dispatch=useDispatch();
+    useEffect(()=>{
+        dispatch(getProduct())
+    },[dispatch])
   return (
     // <div>Home</div>
     <>
+    <PageTitle title="Home"/>
     <Navbar />
     <ImageSlider />
     <div className='home-container'>
