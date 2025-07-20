00d3694e5d6b66693844bf506a7d91169d844de2
dhruvg24 <dhruvgarg24112002@gmail.com>
2025-07-20 13:47:48 +0530

    home page ratings and image slider

---
+++ frontend/src/components/Ratings.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import '../componentStyles/Rating.css'
+const Ratings = ({value,onRatingChange,disabled}) => {
+  const [hoveredRating, setHoveredRating] = useState(0);
+  const [selectedRating, setSelectedRating] = useState(value||0);
+
+  // handling hovering the star(rating)
+  const handleMouseEnter = (rating)=>{
+    if(!disabled){
+      setHoveredRating(rating);
+    }
+  }
+
+  const handleMouseLeave = ()=>{
+    if(!disabled){
+      setHoveredRating(0)
+    }
+  }
+
+  // handle click
+  const handleClick = (rating)=>{
+    if(!disabled){
+      setSelectedRating(rating);
+      // for parent component
+      if(onRatingChange){
+        onRatingChange(rating);
+      }
+    }
+  }
+
+  // generating stars based on selected rating
+  const generateStars = ()=>{
+    const stars = [];
+    for(let i =0;i<5;i++){
+      const isFilled = i<=(hoveredRating || selectedRating)
+      // till hovered rating stars are marked with filled, else with empty class
+      stars.push(
+        <span key={i} className={`star ${isFilled?'filled':'empty'}`}
+        onMouseEnter={()=>handleMouseEnter} onMouseLeave={()=>handleMouseLeave} onClick={()=>handleClick} style={{pointerEvents: disabled?'none':'auto'}}>
+        ★
+        </span>
+      )
+    }
+    return stars;
+
+  }
+  return (
+    <div>
+      <div className='rating'>
+        {generateStars()}
+      </div>
+    </div>
+  )
+}
+
+export default Ratings
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,95 @@ import Footer from '../components/Footer'
 import '../pageStyles/Home.css'
 import Navbar from '../components/Navbar'
 import ImageSlider from '../components/ImageSlider'
+import Product from '../components/Product'
+
+const products= [
+        {
+            "_id": "68779004414a288bc475f2dd",
+            "name": "Product 1",
+            "description": "Product Description 1",
+            "price": 300,
+            "ratings": 4,
+            "image": [
+                {
+                    "public_id": "test_id",
+                    "url": "test_url",
+                    "_id": "68779004414a288bc475f2de"
+                }
+            ],
+            "category": "test category 1",
+            "stock": -2,
+            "numOfReviews": 1,
+            "reviews": [
+                {
+                    "user": "6878fe27aead1c993eaeab7b",
+                    "name": "Dhruv Garg",
+                    "rating": 4,
+                    "comment": "awesome",
+                    "_id": "687a17e8c5459c6e5bcfc65a"
+                }
+            ],
+            "createdAt": "2025-07-16T11:41:56.244Z",
+            "__v": 1
+        },
+        {
+            "_id": "6878f106e8011276e841cd57",
+            "name": "Product 2",
+            "description": "Product Description 2",
+            "price": 400,
+            "ratings": 0,
+            "image": [
+                {
+                    "public_id": "test_id_2",
+                    "url": "test_url_2",
+                    "_id": "6878f106e8011276e841cd58"
+                }
+            ],
+            "category": "trouser",
+            "stock": 1,
+            "numOfReviews": 0,
+            "reviews": [
+                {
+                    "name": "test user 2",
+                    "rating": 3,
+                    "comment": "test comment xyz",
+                    "_id": "6878f106e8011276e841cd59"
+                }
+            ],
+            "user": "6878bb86e88908875662cb32",
+            "createdAt": "2025-07-17T12:48:06.714Z",
+            "__v": 0
+        },
+        {
+            "_id": "6879186ce3de0fe746c79a75",
+            "name": "Product 3",
+            "description": "Product Description 3",
+            "price": 600,
+            "ratings": 0,
+            "image": [
+                {
+                    "public_id": "test_id_3",
+                    "url": "test_url_3",
+                    "_id": "6879186ce3de0fe746c79a76"
+                }
+            ],
+            "category": "shirts",
+            "stock": 1,
+            "numOfReviews": 0,
+            "reviews": [
+                {
+                    "name": "test user 1",
+                    "rating": 4,
+                    "comment": "test comment",
+                    "_id": "6879186ce3de0fe746c79a77"
+                }
+            ],
+            "user": "6878fe27aead1c993eaeab7b",
+            "createdAt": "2025-07-17T15:36:12.070Z",
+            "__v": 0
+        },
+        
+    ]
 
 const Home = () => {
   return (
@@ -12,6 +101,14 @@ const Home = () => {
     <ImageSlider />
     <div className='home-container'>
         <h2 className='home-heading'>Trending Now</h2>
+        <div className='home-product-container'>
+            {
+                products.map((product,index)=>(
+                        <Product product={product} key={index}/>
+                ))
+            }
+            
+        </div>
         <Footer />
     </div>
     </>
